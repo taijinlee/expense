407afcfc72b2a896b98256f5d552ced0b1517a10
Tai-Jin Lee <taijin@gmail.com>
2013-12-12 18:53:13 -0800

    app + config initial files

---
+++ app/server.js
@@ -0,0 +1,80 @@
+
+if (process.env.NODE_ENV === undefined) { throw new Error('NODE_ENV not set. Try \'prod\' or \'dev\'.'); }
+if (process.env.APP_ROOT === undefined) { throw new Error('APP_ROOT not set. Try ~/expense or /service/expense'); }
+
+var express = require('express');
+var config = require(process.env.APP_ROOT + '/config/config.js')();
+var logger = require(process.env.APP_ROOT + '/lib/logger.js')();
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+// overwriting default date token definition
+express.logger.token('date', function() { return Date().replace(/GMT-\d{4} /, ''); });
+
+app.configure(function() {
+  app.use(express.compress());
+  app.use(express.bodyParser({ uploadDir: process.env.APP_ROOT + '/tmp' }));
+  app.use(express.cookieParser());
+  // use this as _method = POST / PUT / DELETE in forms to emulate them without going through backbone
+  app.use(express.methodOverride());
+});
+
+// services
+var datastore = 'mongo';
+var store = require(process.env.APP_ROOT + '/store/store.js')(datastore, config.store.mongo);
+var history = require(process.env.APP_ROOT + '/history/history.js')(store);
+
+// globally applied middleware
+var authMiddleware = require(process.env.APP_ROOT + '/app/middleware/auth.js')(store);
+
+// load config based on environment
+// specific to development
+app.configure('dev', function () {
+  process.env.WEB_ROOT = '/web';
+
+  app.use(express.logger(logger.serverLogFormatDev()));
+  app.use(function(req, res, next) {
+    // filter out VERSION placeholder on dev
+    if (req.url.indexOf('VERSION') !== -1) { req.url = req.url.replace(/VERSION\//, ''); }
+    return next();
+  });
+  app.use(express['static'](process.env.APP_ROOT + process.env.WEB_ROOT));
+  app.use(function(req, res, next) {
+    res.locals.responder = require(process.env.APP_ROOT + '/lib/responder.js')();
+    res.locals.responder.initialize(res);
+    return next();
+  });
+
+  app.use(authMiddleware.getTokenUserId);
+  app.use(app.router);
+  app.use(function(error, req, res, next) { res.locals.responder.send(error); });
+});
+
+// specific to production
+app.configure('prod', function () {
+  process.env.WEB_ROOT = '/web-build';
+
+  app.use(express.logger(logger.serverLogFormat()));
+  app.use(express['static'](process.env.APP_ROOT + process.env.WEB_ROOT, {
+    maxAge: 31536000000 // one year
+  }));
+
+  app.use(function(req, res, next) {
+    res.locals.responder = require(process.env.APP_ROOT + '/lib/responder.js')();
+    res.locals.responder.initialize(res);
+    return next();
+  });
+
+  app.use(authMiddleware.getTokenUserId);
+  app.use(app.router);
+  app.use(function(error, req, res, next) { res.locals.responder.send(error); });
+});
+
+// load routes
+require(process.env.APP_ROOT + '/app/routes.js')(app, store, history);
+require(process.env.APP_ROOT + '/app/sockets.js')(store, history, io);
+// start listening
+server.listen(config.app.port);
+
+logger.log({ message: 'server start', port: config.app.port });
---
+++ config/dev.js
@@ -0,0 +1,48 @@
+module.exports = {
+  app: {
+    port: 4000,
+    host: 'localhost:4000'
+  },
+
+  store: {
+    mysql: {
+      host: 'localhost',
+      port: 3306,
+      maxMillisOnWarn: 5000
+    }
+  },
+
+/*
+  googleOAuth: {
+    clientId: '669677576804-oiqgaqu5qao3606jlqpjij2vln6l4sh5.apps.googleusercontent.com',
+    secret: 'vQbQB798l2q08UmhwfJADzW-',
+    contactScope: 'https://www.google.com/m8/feeds',
+    redirectUri: 'http://localhost:4000/api/auth/google/oauth2callback'
+  },
+
+  facebookOAuth: {
+    clientId: '266039076862542',
+    secret: '3fc1f4abc2677b1afc5039cf6cda8db3',
+    responseType: 'token',
+    contactScope: '',
+    redirectUri: 'http://localhost:4000/api/auth/facebook/oauthcallback'
+  },
+
+  smtp: {
+    service: 'Gmail',
+    user: 'weddingbot@apricotwhisk.com',
+    password: 'ApriC0tWh1sk'
+  },
+
+  aws: {
+    accessKeyId: '',
+    secretAccessKey: '',
+    region: 'us-west-2',
+    sslEnabled: false,
+    s3: {
+      bucket: 'apricotwhisk.com'
+    }
+  }
+*/
+
+};
